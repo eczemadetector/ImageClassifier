dc989c0df74719b0a904c934973aa386264a0675
HuizeMao <38351685+HuizeMao@users.noreply.github.com>
2019-01-27 22:22:02 +0100

    Prediction file used in application

---
+++ predict.py
@@ -0,0 +1,14 @@
+from keras.models import load_model
+import cv2
+import numpy as np
+
+#read image and reshape
+img = cv2.imread('InputImage.jpg')
+img = cv2.resize(img,(128,128))
+img = np.expand_dims(img,axis=0)
+
+#load model and predict
+model = load_model('ResNet50.h5')
+classes = model.predict(img)
+
+print (classes)
