fc79a46c80dc45d6355358734bb849e0d62147bc
HuizeMao <38351685+HuizeMao@users.noreply.github.com>
2019-01-27 22:23:31 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 #read image and reshape
-img = cv2.imread('InputImage.jpg')
+img = cv2.imread('InputImage.png')
 img = cv2.resize(img,(128,128))
 img = np.expand_dims(img,axis=0)
 
