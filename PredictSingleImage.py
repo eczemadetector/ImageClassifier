5f6c01202a5fe5ac35fde09ccf285451e9f3bdd6
spaceface777 <spaceface7777@gmail.com>
2019-01-28 16:18:17 +0100

    Updates

--- PredictSingleImage.py
+++ PredictSingleImage.py
@@ -1,11 +1,22 @@
 import numpy as np
+import sys
+import os
 import cv2
-from keras.models import Model,load_model
 from PIL import Image
+
+# Hide useless warnings
+stderr = sys.stderr
+sys.stderr = open(os.devnull, 'w')
+
+from keras.models import Model,load_model
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
+# Hide useless warnings
+sys.stderr = stderr
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 #define the input file
-file = "InputImage.jpg"
+file = sys.argv[1]
 #load trained model
 model = load_model('ResNet50.h5')
 
@@ -15,9 +26,6 @@ input_image = load_img(file)  # this is a PIL image
 input_image = img_to_array(input_image)
 input_image = input_image.reshape(1,128,128,3)
 
-def predict(img):
-    pred = model.predict(img)
-    print("Prediction of the input image: " + str(pred))
-    return pred
-
-prediction = predict(input_image)
+# Run prediction algorithm and print results
+pred = model.predict(input_image)
+print(int(pred[0]))
