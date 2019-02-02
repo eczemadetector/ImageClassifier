9d74a1f874cf7f2ae0a523cbc57f5ac6e633dae9
HuizeMao <38351685+HuizeMao@users.noreply.github.com>
2019-02-02 14:53:16 +0100

    Add files via upload

--- TestAccuracy.py
+++ TestAccuracy.py
@@ -4,21 +4,25 @@ import h5py
 import cv2
 import keras
 from keras.models import Model, load_model
+import sys
+np.set_printoptions(threshold=sys.maxsize)
 
 #load test set labeled pairs
 X_test = np.load('dataset/X_test.npy')
 Y_test = np.load('dataset/Y_test.npy')
 Y_test = np.array([Y_test]).T
+
+X_test = X_test/255.
 print("Dataset loaded")
 print("Input data shape: " + str(X_test.shape))
 print("labeled data(Y) shape: " + str(Y_test.shape))
 
 #load the trained model
-model = load_model('ResNet50.h5')
+model = load_model('ResNet50_7.h5')
 print("Trained model loaded.")
-
+answers = model.predict(X_test)
+print(answers)
 #evaluate the accuracy of the model
 TestAccuracy = model.evaluate(X_test, Y_test)
 print ("Loss = " + str(TestAccuracy[0]))
 print ("Test Accuracy = " + str(TestAccuracy[1]))
-model.summary()
